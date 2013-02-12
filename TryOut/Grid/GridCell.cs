336c1614f78297298c9c2ed21a567d3aee14c18c
Lirianer <gabin96@hotmail.com>
2013-02-12 05:02:43 -0200

    Blue Shades!

--- TryOut/Grid/GridCell.cs
+++ TryOut/Grid/GridCell.cs
@@ -42,17 +42,51 @@ namespace TryOut.Grid
             g.DrawRectangle(new Pen(new SolidBrush(Color.Black)), rectangle);
             if (this.oldAmount > 0F)
             {
-                if (counter % 2 == 0)
+                if (this.oldAmount > 9)
                 {
-                    g.FillRectangle(new SolidBrush(Color.Red), rectangle);
-                    counter++;
+                    g.FillRectangle(new SolidBrush(ColorTranslator.FromHtml("#08088A")), rectangle);
                 }
-                else
+                else if (8 < this.oldAmount && this.oldAmount < 9)
                 {
-                    g.FillRectangle(new SolidBrush(Color.Green), rectangle);
-                    counter++;
+                    g.FillRectangle(new SolidBrush(ColorTranslator.FromHtml("#0101DF")), rectangle);
+                }
+                else if (7 < this.oldAmount && this.oldAmount < 8)
+                {
+                    g.FillRectangle(new SolidBrush(ColorTranslator.FromHtml("#0000FF")), rectangle);
+                }
+                else if (6 < this.oldAmount && this.oldAmount < 7)
+                {
+                    g.FillRectangle(new SolidBrush(ColorTranslator.FromHtml("#2E2EFE")), rectangle);
+                }
+                else if (5 < this.oldAmount && this.oldAmount < 6)
+                {
+                    g.FillRectangle(new SolidBrush(ColorTranslator.FromHtml("#5858FA")), rectangle);
+                }
+                else if (4 < this.oldAmount && this.oldAmount < 5)
+                {
+                    g.FillRectangle(new SolidBrush(ColorTranslator.FromHtml("#8181F7")), rectangle);
+                }
+                else if (3 < this.oldAmount && this.oldAmount < 4)
+                {
+                    g.FillRectangle(new SolidBrush(ColorTranslator.FromHtml("#A9A9F5")), rectangle);
+                }
+                else if (2 < this.oldAmount && this.oldAmount < 3)
+                {
+                    g.FillRectangle(new SolidBrush(ColorTranslator.FromHtml("#CECEF6")), rectangle);
+                }
+                else if (1 < this.oldAmount && this.oldAmount < 2)
+                {
+                    g.FillRectangle(new SolidBrush(ColorTranslator.FromHtml("#E0E0F8")), rectangle);
+                }
+                else if (0 < this.oldAmount && this.oldAmount < 1)
+                {
+                    g.FillRectangle(new SolidBrush(ColorTranslator.FromHtml("#EFEFFB")), rectangle);
                 }
                 g.DrawString(this.oldAmount.ToString("0.#"), new Font("Arial", 8), new SolidBrush(Color.Blue), rectangle, stringFormat);
+            }
+            else
+            {
+                g.FillRectangle(new SolidBrush(Color.White), rectangle);
             }
         }
         
--- TryOut/Grid/MainGrid.cs
+++ TryOut/Grid/MainGrid.cs
@@ -59,7 +59,7 @@ namespace TryOut.Grid
                     }
                     else if ((i == xCells / 2) && (j == xCells / 2))
                     {
-                        grid[i, j] = new GridCell(gridCellSide, gridCellX + 2, gridCellY + 2, 100F);
+                        grid[i, j] = new GridCell(gridCellSide, gridCellX + 2, gridCellY + 2, 600F);
                     }
                     else
                     {
