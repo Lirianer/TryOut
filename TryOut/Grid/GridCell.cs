5fc1ce31d9854092d2ae97be66dcef4bfbb0a38b
UpperKEES <upperkees@xs4all.nl>
2013-02-14 02:53:05 +0100

    Green color for AC

--- TryOut/Grid/GridCell.cs
+++ TryOut/Grid/GridCell.cs
@@ -13,8 +13,7 @@ namespace TryOut.Grid
        public Rectangle rectangle;
        public double oldAmount, newAmount;
        public bool isWall;
-       double shade = 0;//(255 - color.R) * this.oldAmount + color.R;
-       double blue = 255;//(255 - color.R) * this.oldAmount + color.B;
+       double shade = 0;
        public int X, Y;
        public bool displayAmount = true;
        public bool isSelected = false;
@@ -43,19 +42,30 @@ namespace TryOut.Grid
 
         public virtual void DrawCell(Graphics g)
         {
-            if (oldAmount >= 0.1)
+            const int maxColor = 255;
+            bool isAC = oldAmount < 0;
+            double absAmount = Math.Abs(oldAmount);
+
+            if (absAmount > 0.001)
             {
-                shade = 255 - ((Math.Log10(oldAmount)+1)/4 * 255);
-                if (shade > 255)
+                shade = maxColor - ((Math.Log10(absAmount) + 1) / 4 * maxColor);
+                if (shade > maxColor)
                 {
-                    shade = 255;
+                    shade = maxColor;
                 }
                 if (shade < 0)
                 {
                     shade = 0;
                 }
 
-                color = Color.FromArgb((int)shade, (int)shade, (int)blue);
+                if (isAC)
+                {   // green
+                    color = Color.FromArgb((int)shade, maxColor, (int)shade);
+                }
+                else
+                {   // blue
+                    color = Color.FromArgb((int)shade, (int)shade, maxColor);
+                }
 
                 StringFormat stringFormat = new StringFormat();
                 stringFormat.Alignment = StringAlignment.Center;      // Horizontal Alignment
@@ -65,7 +75,7 @@ namespace TryOut.Grid
 
                 if (displayAmount)
                 {
-                    g.DrawString(this.oldAmount.ToString("0.#"), new Font("Arial", 7), new SolidBrush(Color.Black), rectangle, stringFormat);
+                    g.DrawString(absAmount.ToString("0.#"), new Font("Arial", 7), new SolidBrush(Color.Black), rectangle, stringFormat);
                 }
             }
             if (!isSelected)
